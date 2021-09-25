94359c86466bdf028f7796559ee3c0785468d555
gbaoye <gbaoye@hotmail.com>
2021-09-25 21:55:14 +0800

    Update OpenWrt-Lean.sh

--- OpenWrt-Lean.sh
+++ OpenWrt-Lean.sh
@@ -196,7 +196,7 @@ cat >> .config <<EOF
 #CONFIG_PACKAGE_luci-app-guest-wifi=y
 CONFIG_PACKAGE_luci-app-ttyd=y
 CONFIG_PACKAGE_luci-app-easymesh=y
-CONFIG_PACKAGE_luci-app-smartdns=y
+#CONFIG_PACKAGE_luci-app-smartdns=y
 #CONFIG_PACKAGE_luci-app-webadmin=n #Web管理页面设置
 #CONFIG_PACKAGE_luci-app-ddns=y #DDNS服务
 CONFIG_PACKAGE_luci-app-vlmcsd=n #KMS激活服务器
