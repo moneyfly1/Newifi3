4580f793f8883bd248616683a2ac7e44660b4e10
gbaoye <gbaoye@hotmail.com>
2021-09-29 15:18:34 +0800

    Update OpenWrt-Lean++.sh

--- OpenWrt-Lean++.sh
+++ OpenWrt-Lean++.sh
@@ -81,23 +81,23 @@ CONFIG_TARGET_ramips_mt7621_DEVICE_d-team_newifi-d2=y
 EOF
 
 # 无线驱动(开源)
-cat >> .config <<EOF
+#cat >> .config <<EOF
 #CONFIG_PACKAGE_kmod-mt76=y
-CONFIG_PACKAGE_wireless-regdb=y
-CONFIG_PACKAGE_kmod-cfg80211=y
-CONFIG_PACKAGE_kmod-mac80211=y
-CONFIG_PACKAGE_MAC80211_DEBUGFS=y
-CONFIG_PACKAGE_MAC80211_MESH=y
+#CONFIG_PACKAGE_wireless-regdb=y
+#CONFIG_PACKAGE_kmod-cfg80211=y
+#CONFIG_PACKAGE_kmod-mac80211=y
+#CONFIG_PACKAGE_MAC80211_DEBUGFS=y
+#CONFIG_PACKAGE_MAC80211_MESH=y
 #use opensource wifi driver
-CONFIG_PACKAGE_kmod-mt7603=y
-CONFIG_PACKAGE_kmod-mt7603e=n
-CONFIG_PACKAGE_kmod-mt76x2=y
-CONFIG_PACKAGE_kmod-mt76x2-common=y
-CONFIG_PACKAGE_kmod-mt76x2e=n
-CONFIG_PACKAGE_wpad-openssl=y
-CONFIG_PACKAGE_wpa-supplicant=y
-CONFIG_PACKAGE_luci-app-mtwifi=n
-EOF
+#CONFIG_PACKAGE_kmod-mt7603=y
+#CONFIG_PACKAGE_kmod-mt7603e=n
+#CONFIG_PACKAGE_kmod-mt76x2=y
+#CONFIG_PACKAGE_kmod-mt76x2-common=y
+#CONFIG_PACKAGE_kmod-mt76x2e=n
+#CONFIG_PACKAGE_wpad-openssl=y
+#CONFIG_PACKAGE_wpa-supplicant=y
+#CONFIG_PACKAGE_luci-app-mtwifi=n
+#EOF
 
 # IPv6支持:
 cat >> .config <<EOF
