596a41e5b0ac605c8122d0c3303d72534d0f05e4
gbaoye <gbaoye@hotmail.com>
2021-09-29 17:06:42 +0800

    Update OpenWrt-Lean++.sh

--- OpenWrt-Lean++.sh
+++ OpenWrt-Lean++.sh
@@ -83,17 +83,17 @@ EOF
 # 无线驱动(开源)
 cat >> .config <<EOF
 #CONFIG_PACKAGE_kmod-mt76=y
-CONFIG_PACKAGE_wireless-regdb=y
-CONFIG_PACKAGE_kmod-cfg80211=y
-CONFIG_PACKAGE_kmod-mac80211=y
-CONFIG_PACKAGE_MAC80211_DEBUGFS=y
-CONFIG_PACKAGE_MAC80211_MESH=y
+#CONFIG_PACKAGE_wireless-regdb=y
+#CONFIG_PACKAGE_kmod-cfg80211=y
+#CONFIG_PACKAGE_kmod-mac80211=y
+#CONFIG_PACKAGE_MAC80211_DEBUGFS=y
+#CONFIG_PACKAGE_MAC80211_MESH=y
 #use opensource wifi driver
 CONFIG_PACKAGE_kmod-mt7603=y
 CONFIG_PACKAGE_kmod-mt7603e=n
 CONFIG_PACKAGE_kmod-mt76x2=y
-CONFIG_PACKAGE_kmod-mt76x2-common=y
 CONFIG_PACKAGE_kmod-mt76x2e=n
+CONFIG_PACKAGE_hostapd-common=y
 CONFIG_PACKAGE_wpad-openssl=y
 CONFIG_PACKAGE_wpa-supplicant=y
 CONFIG_PACKAGE_luci-app-mtwifi=n
