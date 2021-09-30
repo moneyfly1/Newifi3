6233877a9e8cc9a33d3474780c65af44929a1a4b
gbaoye <gbaoye@hotmail.com>
2021-09-30 17:20:55 +0800

    Update OpenWrt-Boos4721.sh

--- OpenWrt-Boos4721.sh
+++ OpenWrt-Boos4721.sh
@@ -28,10 +28,10 @@ cat feeds.conf.default
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/hubbylei/luci-app-clash package/luci-app-clash
-#git clone https://github.com/joyplayer/luci-app-clash-joy package/luci-app-clash-joy
+git clone https://github.com/110010110/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-smartdns.git package/luci-app-smartdns
 #git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 #git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-openclash package/luci-app-openclash
