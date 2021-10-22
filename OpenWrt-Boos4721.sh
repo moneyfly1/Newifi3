b96c3e236076cb9478409d6e1c4b7093200e3a6d
gbaoye <gbaoye@hotmail.com>
2021-10-22 10:41:10 +0800

    Update OpenWrt-Boos4721.sh

--- OpenWrt-Boos4721.sh
+++ OpenWrt-Boos4721.sh
@@ -94,13 +94,13 @@ CONFIG_PACKAGE_kmod-mt76=y
 #CONFIG_PACKAGE_MAC80211_DEBUGFS=y
 #CONFIG_PACKAGE_MAC80211_MESH=y
 #use opensource wifi driver
-CONFIG_PACKAGE_kmod-mt7603=y
-CONFIG_PACKAGE_kmod-mt7603e=n
-CONFIG_PACKAGE_kmod-mt76x2=y
-CONFIG_PACKAGE_kmod-mt76x2e=n
-CONFIG_PACKAGE_hostapd-common=y
-CONFIG_PACKAGE_wpad-openssl=y
-CONFIG_PACKAGE_wpa-supplicant=y
+#CONFIG_PACKAGE_kmod-mt7603=y
+#CONFIG_PACKAGE_kmod-mt7603e=n
+#CONFIG_PACKAGE_kmod-mt76x2=y
+#CONFIG_PACKAGE_kmod-mt76x2e=n
+#CONFIG_PACKAGE_hostapd-common=y
+#CONFIG_PACKAGE_wpad-openssl=y
+#CONFIG_PACKAGE_wpa-supplicant=y
 EOF
 
 # IPv6支持:
