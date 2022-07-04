c0aab17ea42c5352b87039e322c097e845d10704
gbaoye <gbaoye@hotmail.com>
2022-07-04 11:58:49 +0800

    Update OpenWrt-Boos4721.sh

--- OpenWrt-Boos4721.sh
+++ OpenWrt-Boos4721.sh
@@ -9,7 +9,7 @@ cd openwrt
 # 更新feeds文件
 #sed -i 's@#src-git helloworld@src-git helloworld@g' feeds.conf.default #启用helloworld
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages' feeds.conf.default
+#sed -i '$a src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 cat feeds.conf.default
 
