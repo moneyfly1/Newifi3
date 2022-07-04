c1c147b904fa5636678e99302c68237830cafd60
gbaoye <gbaoye@hotmail.com>
2022-07-04 17:43:52 +0800

    Update OpenWrt-Lean+.sh

--- OpenWrt-Lean+.sh
+++ OpenWrt-Lean+.sh
@@ -7,12 +7,12 @@ cd openwrt
 #ln -s ../../diy ./package/openwrt-packages
 
 # 更新feeds文件
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i 's@#src-git helloworld@src-git helloworld@g' feeds.conf.default #启用helloworld
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git Boos4721 https://github.com/Boos4721/OpenWrt-Packages' feeds.conf.default
-cat feeds.conf.default
+#cat feeds.conf.default
 
 # 更新并安装源
 ./scripts/feeds clean
